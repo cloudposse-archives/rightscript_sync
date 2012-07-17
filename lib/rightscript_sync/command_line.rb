f0456c740ed490c8740b98ff2b88f107be59a629
osterman <e@osterman.com>
2012-07-17 02:09:40 -0700

    fixed require for optparse

--- lib/rightscript_sync/command_line.rb
+++ lib/rightscript_sync/command_line.rb
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with RightScript Sync.  If not, see <http://www.gnu.org/licenses/>.
 #
-require 'optparser'
+require 'optparse'
 
 module RightScriptSync
   class MissingArgumentException < Exception; end
